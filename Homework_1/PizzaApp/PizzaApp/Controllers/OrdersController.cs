4ad168343cd6c3ad93cf4e82f6ef03bd9069aac8
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-08-14 17:18:01 +0200

    Add files via upload

---
+++ Homework_1/PizzaApp/PizzaApp/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        [Route("Orders")]
+        public IActionResult Index()
+        {
+            List<Order> orders = StaticDb.Orders;
+            return View(orders);
+        }
+        [Route("Orders/Details/{id?")]
+        public IActionResult Details(int? id)
+        {
+            if(id == 0)
+            {
+                return new EmptyResult();
+            }
+            Order order = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+            if(order == null)
+            {
+                return new EmptyResult();
+            }
+            return View(order);
+        }
+        [Route("Orders/JsonData")]
+        public IActionResult JsonReturns()
+        {
+            return new JsonResult(StaticDb.Orders);
+        }
+        public IActionResult RedictToHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Homework_1/PizzaApp/PizzaApp/Models/Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿using PizzaApp.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaApp.Models.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
