cf339986e095103efdaade46a59efd4b687499a0
Elena-r-code <75211054+Elena-r-code@users.noreply.github.com>
2021-08-29 22:57:53 +0200

    Add files via upload

---
+++ Homework_4/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Services/Implementations/UserService.cs
@@ -0,0 +1,27 @@
+﻿using SEDC.PizzaApp.DataAccess.Interfaces;
+using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.Mappers;
+using SEDC.PizzaApp.Services.Interfaces;
+using SEDC.PizzaApp.ViewModels.UserViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PizzaApp.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private IRepository<User> _userRepository;
+        public UserService(IRepository<User> userRepository) 
+        {
+            _userRepository = userRepository;
+        }
+
+        public List<UserDDViewModel> GetUsersForDropdown()
+        {
+            
+            List<User> usersDb = _userRepository.GetAll();
+            
+            return usersDb.Select(x => x.ToUserDDViewModel()).ToList();
+        }
+    }
+}
---
+++ Homework_4/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.ViewModels/PizzaViewModels/PizzaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SEDC.PizzaApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SEDC.PizzaApp.ViewModels.PizzaViewModels
+{
+    public class PizzaViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Pizza Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Price")]
+        public int Price { get; set; }
+
+        [Display(Name = "Pizza Size")]
+        public PizzaSizeEnum PizzaSize { get; set; }
+
+        [Display(Name = "Is On Promotion")]
+        public bool IsOnPromotion { get; set; }
+        
+
+  
+    }
+}
